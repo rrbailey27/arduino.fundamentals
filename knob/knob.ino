df472474ddbe55969cb3918c238075646fa2d0dd
GCL <lewin.gc@gmail.com>
2018-10-02 08:24:16 -0400

    updated delay

--- knob/knob.ino
+++ knob/knob.ino
@@ -15,6 +15,6 @@ void loop()
   int adcValue = analogRead(potPin);          // reads the value of the potentiometer (value between 0 and 1023)
   int angle = map(adcValue, 0, 1023, 0, 180); // scale it to use it with the servo (value between 0 and 180)
   myservo.write(angle);                       // sets the servo position according to the scaled value
-  delay(15);                                  // waits for the servo to get there
+  delay(200);                                  // waits for the servo to get there
 }
 
